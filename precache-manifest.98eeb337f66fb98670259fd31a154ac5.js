c783394bd25c08435d871965bb4aff945477a139
Fiddlekins <fiddlekins@gmail.com>
2020-02-17 18:58:46 +0000

    Updates

---
+++ precache-manifest.98eeb337f66fb98670259fd31a154ac5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7b54ab0a578d274f5e2f6b3e370bc93",
+    "url": "/akun-portal/index.html"
+  },
+  {
+    "revision": "da8e516cf51cf638fc71",
+    "url": "/akun-portal/static/css/main.50080ca5.chunk.css"
+  },
+  {
+    "revision": "3a2a1850a62e0d010ccd",
+    "url": "/akun-portal/static/js/2.ede4e652.chunk.js"
+  },
+  {
+    "revision": "16eb2272a4cd96cdbd1b27fe1aade0b2",
+    "url": "/akun-portal/static/js/2.ede4e652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da8e516cf51cf638fc71",
+    "url": "/akun-portal/static/js/main.2f81978a.chunk.js"
+  },
+  {
+    "revision": "a28dfdae4719b3d748ab",
+    "url": "/akun-portal/static/js/runtime-main.45502f15.js"
+  },
+  {
+    "revision": "20b1eba6de409828a6d8f947db8357ec",
+    "url": "/akun-portal/static/media/spiral.20b1eba6.svg"
+  }
+]);
